1ce4625886602f01b8b98bc2a61d40e1216dc475
Alia Borroho <aliaborroho@gmail.com>
2015-07-20 16:08:54 -0500

    gh pages 2nd try

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,20 @@
-//lalalala//
+var elem = document.getElementById('clock');
+
+function updateClock() {
+   var now = new Date();
+   var hours = now.getHours();
+   var minutes = now.getMinutes();
+   var seconds = now.getSeconds();
+
+   if (hours < 10) {
+       hours = "0" + hours;
+           }
+   if (minutes < 10) {
+       minutes = "0" + minutes;
+           }
+   if (seconds < 10) {
+       seconds = "0" + seconds;
+           }
+   elem.innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+
